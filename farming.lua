8d622d358cc4b58642ea16134ae2e39a4f1df34a
benjamenhuynh <bhhuynh@uwm.edu>
2026-03-19 20:20:18 -0500

    adding stuff

--- farming.lua
+++ farming.lua
@@ -7,19 +7,41 @@
 ------------------------
 --- CONFIG: 
 ------------------------
-local function main()
-    local block = turtle.inspectDown();
-    if(block and block.age == 7)then
-        turtle.digDown();
-        turtle.select(2);
-        turtle.placeDown();
-    else if block and block.name == "minecraft::red_wool" then
-        turtle.dance();
-    end
-    turtle.forward()
+function main()
+    checkFuel();
+    checkBlock();
+
 end
 
-
-while true do
-    main()
+function checkBlock()
+    local isBlock, block = turtle.inspectDown();
+   
+    if isBlock then
+        print("Block found" .. block.name);
+        -- make an array of blocks that are considered farmland check the array 
+        -- need a second pair of arrays that check if the block is a colored wool 
+        if block.name == "minecraft:carrots" then
+            print("Block is farmland.");
+            
+        else
+            print("Block is not farmland: " .. data.name);
+            return false;
+        end
+    else
+        print("No block detected.");
+        turtle.suckDown();
+        turtle.placeDown();
+        turtle.forward();
+        return false;
+    end
+end
+function checkFuel()s
+    local fuelLevel = turtle.getFuelLevel();
+    if fuelLevel < 200 then
+        print("Low fuel: " .. fuelLevel);
+        turtle.select(1);
+        turtle.refuel(1);
+    else
+        print("Fuel level is sufficient: " .. fuelLevel);
+    end
 end
