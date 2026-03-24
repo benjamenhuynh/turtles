20d2a1feb865d5492918b58d7f60c245be9a658d
benjamenhuynh <bhhuynh@uwm.edu>
2026-03-23 23:51:10 -0500

    need to update from tekkit later

--- farming.lua
+++ farming.lua
@@ -7,41 +7,105 @@
 ------------------------
 --- CONFIG: 
 ------------------------
-function main()
-    checkFuel();
-    checkBlock();
+local farmArr = {"minecraft:carrots", "minecraft:potatoes", "minecraft:wheat"}
+local woolPairs = { {"white", "red"}}
+--, {"red", "yellow"}, {"yellow", "gray"} 
+local startingBlockIndex = 1
 
+
+function main()
+    checkFuel()
+    checkBlock()
 end
+
 
 function checkBlock()
-    local isBlock, block = turtle.inspectDown();
-   
+    local isBlock, block = turtle.inspectDown()
     if isBlock then
-        print("Block found" .. block.name);
-        -- make an array of blocks that are considered farmland check the array 
-        -- need a second pair of arrays that check if the block is a colored wool 
-        if block.name == "minecraft:carrots" then
-            print("Block is farmland.");
-            
-        else
-            print("Block is not farmland: " .. data.name);
-            return false;
+        print("Block found" .. block.name)
+
+        for i, pair in ipairs(woolPairs) do
+            local foundOrange = false
+            while not foundOrange do
+                if block.name == "minecraft:wool" then
+                    if block.state.color == "orange" then
+                        print("Block is orange wool - chest found!")
+                        findChest()
+                        foundOrange = true
+                    elseif block.state.color == pair[1] then
+                        print("Block is starting wool: " .. block.state.color)
+                        -- starting turns
+                        turtle.turnRight()
+                        turtle.forward()
+                        turtle.turnRight()
+                        turtle.forward()
+                    elseif block.state.color == pair[2] then
+                        print("Block is ending wool: " .. block.state.color)
+                        -- ending turns
+                        turtle.turnLeft()
+                        turtle.forward()
+                        turtle.turnLeft()
+                        turtle.forward()
+                    else
+                        for i = 1, #farmArr do
+                            if block.name == farmArr[i] and block.state.age == 7 then
+                                turtle.dig()
+                                turtle.placeDown()     
+                            end
+                        end
+                    end
+                    turtle.forward()
+                end
+            end
         end
     else
-        print("No block detected.");
-        turtle.suckDown();
-        turtle.placeDown();
-        turtle.forward();
-        return false;
+        print("No block detected.")
+        turtle.suckDown()
+        turtle.placeDown()
+        turtle.forward()
     end
 end
-function checkFuel()s
-    local fuelLevel = turtle.getFuelLevel();
+
+
+function checkFuel()
+    local fuelLevel = turtle.getFuelLevel()
     if fuelLevel < 200 then
-        print("Low fuel: " .. fuelLevel);
-        turtle.select(1);
-        turtle.refuel(1);
+        print("Low fuel: " .. fuelLevel)
+        turtle.select(1)
+        turtle.refuel(1)
     else
-        print("Fuel level is sufficient: " .. fuelLevel);
+        print("Fuel level is sufficient: " .. fuelLevel)
     end
+end
+
+
+function emptyInventory()
+    for i = 2, 16 do
+        turtle.select(i)
+        turtle.dropUp()
+    end
+end
+
+
+function findChest()
+    local foundChest = false
+    local isBlock, block = turtle.inspectUp()
+
+    turtle.turnLeft()
+
+    while not foundChest do
+        if isBlock and block.name == "minecraft:chest" then
+            print("Chest found above!")
+            emptyInventory()
+            foundChest = true
+        else
+            isBlock, block = turtle.inspectUp()
+        end
+        turtle.forward()
+    end
+    
+end
+
+while true do
+    main()
 end
